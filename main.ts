f8a7fe236cc29993b0c7dd526c52ccc2030bb04c
c-antin <38494182+c-antin@users.noreply.github.com>
2023-08-22 16:45:10 +0200

    time: better labels

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import { worker_work_heap } from "./mod.ts";
 
 //deno run --allow-read main.ts
 
-console.time("worker_work_heap single");
+console.time("worker_work_heap 1");
 await worker_work_heap();
-console.timeEnd("worker_work_heap single");
+console.timeEnd("worker_work_heap 1");
 
-console.time("worker_work_heap concurrent");
+console.time("worker_work_heap 5 concurrent");
 await Promise.allSettled(Array.from(Array(5)).map(() => worker_work_heap()));
-console.timeEnd("worker_work_heap concurrent");
+console.timeEnd("worker_work_heap 5 concurrent");
 
-console.time("worker_work_heap sequential");
+console.time("worker_work_heap 5 sequential");
 for (let i = 0; i < 5; i++) {
   await worker_work_heap();
 }
-console.timeEnd("worker_work_heap sequential");
+console.timeEnd("worker_work_heap 5 sequential");
